90892651acaac81f2b4dd4411565116a8493b7da
delhoum <ahmed.delhoum@yahoo.fr>
2025-11-16 23:19:19 +0100

    J'ai ajouté un Agent local de supervision

--- agent_local.py
+++ agent_local.py
@@ -5,20 +5,6 @@ import socket
 
 SERVER_URL = "http://127.0.0.1:5000/metrics"   # URL du serveur Flask
 INTERVAL = 10  # intervalle d'envoi en secondes
-
-def collect_metrics():
-    """Collecte les métriques système du poste local."""
-    return {
-        "hostname": socket.gethostname(),
-        "cpu": psutil.cpu_percent(interval=1),
-        "ram": psutil.virtual_memory().percent,
-        "disk": psutil.disk_usage('/').percent,
-        "network_sent": psutil.net_io_counters().bytes_sent,
-        "network_recv": psutil.net_io_counters().bytes_recv
-    }
-
-def send_metrics(data):
-    """Envoie les métriques collectées au serveur central Flask."""
     try:
         requests.post(SERVER_URL, json=data, timeout=5)
         print("[OK] Données envoyées")
@@ -35,3 +21,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def collect_metrics():
+    """Collecte les métriques système du poste local."""
+    return {
+        "hostname": socket.gethostname(),
+        "cpu": psutil.cpu_percent(interval=1),
+        "ram": psutil.virtual_memory().percent,
+        "disk": psutil.disk_usage('/').percent,
+        "network_sent": psutil.net_io_counters().bytes_sent,
+        "network_recv": psutil.net_io_counters().bytes_recv
+    }
+
+def send_metrics(data):
+    """Envoie les métriques collectées au serveur central Flask."""
